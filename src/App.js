53708a886cf08a204f673d01258ee5a6986a6925
lulucodingwizard <how.to.develop.fast@gmail.com>
2024-07-05 13:34:36 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -11,26 +11,54 @@ const App = () => {
   const [chatHistory, setChatHistory] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  // inislize your Gemeni Api
+  const genAI = new GoogleGenerativeAI(
+    "AIzaSyBT7jIJOd1OfvQGnGyCUUFVKh2QtCudpEg"
+  );
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
   // Function to handle user input
   const handleUserInput = (e) => {
     setUserInput(e.target.value);
   };
 
   // Function to send user message to Gemini
-  const sendMessage = async () => {};
+  const sendMessage = async () => {
+    if (userInput.trim() === "") return;
+
+    setIsLoading(true);
+    try {
+      // call Gemini Api to get a response
+      const result = await model.generateContent(userInput);
+      const response = await result.response;
+      console.log(response);
+      // add Gemeni's response to the chat history
+      setChatHistory([
+        ...chatHistory,
+        { type: "user", message: userInput },
+        { type: "bot", message: response.text() },
+      ]);
+    } catch {
+      console.error("Error sending message");
+    } finally {
+      setUserInput("");
+      setIsLoading(false);
+    }
+  };
 
   // Function to clear the chat history
-  const clearChat = () => {};
+  const clearChat = () => {
+    setChatHistory([]);
+  };
 
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold text-center mb-4">Chatbot</h1>
-      {(!chatHistory || isLoading) && (
-        <div className="chat-container rounded-lg shadow-md p-4">
-          <ChatHistory chatHistory={chatHistory} />
-          <Loading isLoading={isLoading} />
-        </div>
-      )}
+
+      <div className="chat-container rounded-lg shadow-md p-4">
+        <ChatHistory chatHistory={chatHistory} />
+        <Loading isLoading={isLoading} />
+      </div>
 
       <div className="flex mt-4">
         <input
